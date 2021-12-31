90ed7b27393932535bab0d528ff275d3c008781a
dosha10 <ndungufrank01@gmail.com>
2021-12-31 10:08:04 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,46 @@
+import SHA256 from "crypto-js/sha256.js";
+
+class Block {
+  constructor(index, timestamp, data, previousHash = "") {
+    this.index = index;
+    this.timestamp = timestamp;
+    this.data = data;
+    this.previousHash = previousHash;
+    this.hash = this.calculateHash();
+  }
+
+  calculateHash() {
+    return SHA256(
+      this.index +
+        this.previousHash +
+        this.timestamp +
+        JSON.stringify(this.data)
+    ).toString();
+  }
+}
+
+class Blockchain {
+  constructor() {
+    this.chain = [this.createGenesisBlock()];
+  }
+
+  createGenesisBlock() {
+    return new Block(0, "01/01/2021", "Genesis Block", "0");
+  }
+
+  getLatestBlock() {
+    return this.chain[this.chain.length - 1];
+  }
+
+  addBlock(newBlock) {
+    newBlock.previousHash = this.getLatestBlock().hash;
+    newBlock.hash = newBlock.calculateHash();
+    this.chain.push(newBlock);
+  }
+}
+
+let doshaCoin = new Blockchain();
+doshaCoin.addBlock(new Block(1, "10/07/2021", { amount: 4 }));
+doshaCoin.addBlock(new Block(2, "12/07/2021", { amount: 10 }));
+
+console.log(JSON.stringify(doshaCoin, null, 4));
